431012d4ca82c67342f7ac0b9848f7ed06da491a
sarowarhosen03 <md.sarowar.hosse@gmail.com>
2024-02-15 15:27:37 +0600

    reafactor code

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 import puppeteer from "puppeteer";
- 
+const DELAY = 30; //type delay in ms 
+const MATCH_DEALY_BEFORE_START = 12000; //WAIT delay in ms 
+const WAIT_AFTER_COMPLETE = 20000; //WAIT delay in ms 
+const TARGET_TO_TYPE_EL = "#gwt-uid-20 > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div > span";//WHERE THE TEXT TO TYPE IS LOCATED
 (async function () {
 
     const browser = await puppeteer.launch({
-        headless: false
+        headless: false,
+        defaultViewport: {
+            width: 756, height: 1080,
+            deviceScaleFactor: 1
+        }
     });
     const page = await browser.newPage();
     page.setDefaultTimeout(0)
-
     await page.goto('https://play.typeracer.com/', {
         waitUntil: 'networkidle0'
     });
     await page.waitForSelector('#gwt-uid-1 > a')
     await page.click('#gwt-uid-1 > a');
-    await page.waitForSelector('#gwt-uid-20 > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div > span:nth-child(3)')
-
-    let toType = await page.$$eval('#gwt-uid-20 > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div > span', (element) => element.map((el) => el.textContent).join(''));
+    await page.waitForSelector(TARGET_TO_TYPE_EL)
+    let toType = await page.$$eval(TARGET_TO_TYPE_EL, (element) => element.map((el) => el.textContent).join(''));
     console.log(toType);
-    await page.waitForSelector('.txtInput');
-    setTimeout(() => {
+    const targetBox = await page.waitForSelector('.txtInput');
+    await targetBox.scrollIntoView();
 
-        startTyping(page);
-    }, 11500);
-    async function startTyping(page) {
-
-        page.keyboard.type(toType, { delay: 60 }); // Types slower, like a user
-
-
-    }
-
-
+    await sleep(MATCH_DEALY_BEFORE_START)
+    await page.keyboard.type(toType, { delay: DELAY }); // Types slower, like a user
+    await sleep(WAIT_AFTER_COMPLETE)
+    await browser.close();
+    await process.exit(0);
 }())
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
